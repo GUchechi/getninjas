911db30dc69fc9a4c0f94dca2c862eb0e5a5faf7
GUchechi <godswilluchendu7@gmail.com>
2024-11-15 11:32:50 +0100

    first commit

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,15 +1,36 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState, useEffect } from "react";
+import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav>
-      <Link className="logo" href="/">
-        <Image src="/logo.png" width={60} height={10} alt="" />
+    <nav
+      className={`${styles.nav} ${isScrolled ? styles.normal : styles.glass}`}
+    >
+      <Link className={styles.logo} href="/">
+        <Image src="/logo.png" width={60} height={10} alt="Site Logo" />
       </Link>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/users">Ninja</Link>
+      <div className={styles.links}>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/users">Ninja</Link>
+      </div>
     </nav>
   );
 };
